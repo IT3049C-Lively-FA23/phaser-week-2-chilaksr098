9a2e3a570035d77b754e98c40bad5c6efc7c6ffa
sasidhar chilaka <97551417+chilaksr098@users.noreply.github.com>
2023-12-07 12:36:56 -0500

    Update game.js

--- game.js
+++ game.js
@@ -19,46 +19,47 @@ class mainScene {
   }
 
   update() {
-    if (this.arrow.right.isDown) {
-      this.player.x += 3;
-    } else if (this.arrow.left.isDown) {
-      this.player.x -= 3;
+        if (this.physics.overlap(this.player, this.dot)) {
+            this.hit();
+        }
+
+        if (this.arrow.right.isDown) {
+            this.player.x += 3;
+        } else if (this.arrow.left.isDown) {
+            this.player.x -= 3;
+        }
+
+        if (this.arrow.down.isDown) {
+            this.player.y += 3;
+        } else if (this.arrow.up.isDown) {
+            this.player.y -= 3;
+        }
     }
 
-    if (this.arrow.down.isDown) {
-      this.player.y += 3;
-    } else if (this.arrow.up.isDown) {
-      this.player.y -= 3;
+
+    hit() {
+        this.dot.x = Phaser.Math.Between(100, 600);
+        this.dot.y = Phaser.Math.Between(100, 300);
+
+        this.score += 10;
+
+        this.scoreText.setText('score: ' + this.score);
+
+        this.tweens.add({
+            targets: this.player,
+            duration: 200,
+            scaleX: 1.2,
+            scaleY: 1.2,
+            yoyo: true,
+        });
     }
-
-    // Check for overlap and call the hit() function
-    if (this.physics.overlap(this.player, this.coin)) {
-      this.hit();
-    }
-  }
-
-  hit() {
-    this.dot.x = Phaser.Math.Between(100, 600);
-    this.dot.y = Phaser.Math.Between(100, 300);
-    this.score += 10;
-    this.scoreText.setText("score: " + this.score);
-
-    // Create a new tween for the player
-    this.tweens.add({
-      targets: this.player,
-      duration: 200,
-      scaleX: 1.2,
-      scaleY: 1.2,
-      yoyo: true,
-    });
-  }
 }
 
 new Phaser.Game({
-  width: 1500,
-  height: 1000,
-  backgroundColor: "#3498db",
-  scene: mainScene,
-  physics: { default: "arcade" },
-  parent: "game",
+    width: 1100,
+    height: 800,
+    backgroundColor: '#50C878',
+    scene: mainScene,
+    physics: { default: 'arcade' },
+    parent: 'game',
 });
